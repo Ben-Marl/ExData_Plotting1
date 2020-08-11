03bf4e1c537cc735b28d113b39871a702bb42896
Your Name <you@example.com>
2020-08-11 15:08:10 +0530

    plot 2

---
+++ plot2.r
@@ -0,0 +1,13 @@
+library(datasets)
+
+df <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
+df$date.time <- as.POSIXct(paste(df$Date, df$Time), format = "%d/%m/%Y %T")
+
+df$Date <- as.Date(df$Date, format="%d/%m/%Y")
+
+sub_df <- subset(df, Date == "2007-02-01" | Date == "2007-02-02")
+
+
+png("plot2.png", width = 480, height = 480)
+plot(sub_df$date.time,as.numeric(as.character(subDf$Global_active_power)), type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
